c70e90a6ee01d4b6f8aa0c0efe1f4a22f727d296
Alessandro Emoticron <aleluigimar@gmail.com>
2018-04-05 21:24:19 +0200

    Funzionalità camera

--- tesiLaureaEmoticron/tesiLaureaEmoticron/ViewController.swift
+++ tesiLaureaEmoticron/tesiLaureaEmoticron/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
+import GoogleMVDataOutput
+import GoogleMobileVision
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+        } catch {
+            print(error)
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
+    
 }
 
